4eef746868889c306243c0ca0dc6f65206b0cf2c
Joseph Flinn <joseph.s.flinn@gmail.com>
2022-11-04 14:06:27 -0700

    [controllers/bw-secrets] The secret creation is working!

---
+++ homelab/controllers/bw-secrets/sync-pod.py
@@ -0,0 +1,37 @@
+from base64 import b64encode, b64decode
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import json
+
+
+class Controller(BaseHTTPRequestHandler):
+    def sync(self, parent, children):
+        # Compute status based on observed state.
+        desired_status = {"secrets": len(children["Secret.v1"])}
+
+        # Generate the desired child object(s).
+        who = parent.get("spec", {}).get("who", "World")
+        desired_pods = [
+            {
+                "apiVersion": "v1",
+                "kind": "Secret",
+                "type": "Opaque",
+                "metadata": {"name": parent["metadata"]["name"]},
+                "data": {"value": f"{b64encode(b'secret').decode('utf-8')}"},
+            }
+        ]
+
+        return {"status": desired_status, "children": desired_pods}
+
+    def do_POST(self):
+        # Serve the sync() function as a JSON webhook.
+        observed = json.loads(self.rfile.read(int(self.headers.get("content-length"))))
+        desired = self.sync(observed["parent"], observed["children"])
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(desired).encode())
+
+
+HTTPServer(("", 80), Controller).serve_forever()
